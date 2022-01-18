928cb94c6ff26f622d66fcd49569a42fecd2cc63
AdamAlibraheem <shrineknight000@gmail.com>
2022-01-18 14:49:38 -0500

    completed HTML generation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const fs = require("fs");
 
 const arr = [];
 
@@ -75,9 +76,7 @@ function baseQuestions() {
         answers.email,
         answers.officeNum
       );
-      console.log(manager);
       arr.push(manager);
-      // testArr();
       if (answers.menu === "engineer") {
         engineerGenerator();
       } else if (answers.menu === "intern") {
@@ -151,9 +150,7 @@ function engineerGenerator() {
         answers.emailEng,
         answers.githubEng
       );
-      console.log(engineer);
       arr.push(engineer);
-      // testArr();
       confirmation();
     });
 }
@@ -222,7 +219,6 @@ function internGenerator() {
         answers.schoolInt
       );
       arr.push(intern);
-      // testArr();
       confirmation();
     });
 }
@@ -238,7 +234,6 @@ function confirmation() {
       },
     ])
     .then((answers) => {
-      console.log(answers);
       if (answers.confirmSelection === true) {
         inquirer
           .prompt([
@@ -266,27 +261,94 @@ function buildHTML() {
   // function to build html
   // For loop of arr to generate constructors
 
+  const starterHTML = baseHTML();
+
+  fs.writeFileSync("./index.html", starterHTML);
+
   for (i = 0; i < arr.length; i++) {
     if (arr[i].getRole() === "Manager") {
-      console.log("Manager");
+      let newRecord = arr[i];
+      let managerHTML = managerCard(newRecord);
+      fs.appendFileSync("./index.html", managerHTML);
     } else if (arr[i].getRole() === "Engineer") {
-      console.log("Engineer");
+      let newRecordEng = arr[i];
+      let engineerHTML = EngineerCard(newRecordEng);
+      fs.appendFileSync("./index.html", engineerHTML);
     } else if (arr[i].getRole() === "Intern") {
-      console.log("Intern");
+      let newRecordInt = arr[i];
+      let internHTML = InternCard(newRecordInt);
+      fs.appendFileSync("./index.html", internHTML);
     }
   }
-  // if arr[i].getRole() === Manager : manager card
-  // if arr[i].getRole() === Engineer : engineer card
-  // if arr[i].getRole() === Intern : intern card
-  // console.log("Build HTML");
-  // return ``;
+
+  const footerData = footerHTML();
+
+  fs.appendFileSync("./index.html", footerData);
 }
 
-function testArr() {
-  for (i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-  }
+function baseHTML() {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Team Profile Generator</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  </head>
+  <body> 
+  <header class="bg-primary h1 py-5 text-white text-center">Profile Generator </header> 
+  <div class="container">
+
+  `;
+}
+
+function footerHTML() {
+  return `
+
+  </div>
+   </body>
+   </html>`;
+}
+
+function managerCard(newRecord) {
+  return `
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h5 class="card-title">Manager</h5>
+      <h5 class="card-title">${newRecord.name}</h5>
+      <a class="card-text" href="www.${newRecord.email}.com" target="_blank" >${newRecord.email}</a>
+      <p class="card-text">${newRecord.officeNumber}</p>
+    </div>
+  </div>`;
+}
+
+function EngineerCard(newRecordEng) {
+  return `
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h5 class="card-title">Engineer</h5>
+      <h5 class="card-title">${newRecordEng.name}</h5>
+      <p class="card-text">ID number: ${newRecordEng.id}</p>
+      <a class="card-text" href="${newRecordEng.email}">${newRecordEng.email}</a>
+      <br>
+      <a class="card-text"href="${newRecordEng.github}">${newRecordEng.github}</a>
+    </div>
+  </div>`;
+}
+
+function InternCard(newRecordInt) {
+  return `
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h5 class="card-title">Intern</h5>
+      <h5 class="card-title">${newRecordInt.name}</h5>
+      <p class="card-text">ID number: ${newRecordInt.id}</p>
+      <a class="card-text" href="${newRecordInt.email}">${newRecordInt.email}</a>
+      <br>
+      <a class="card-text" href="${newRecordInt.school}">${newRecordInt.school}</a>
+    </div>
+  </div>`;
 }
 
 baseQuestions();
-// engineerGenerator();
